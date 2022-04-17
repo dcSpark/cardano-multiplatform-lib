332cba58298432f3a1d9762f306725efc3132548
Sebastien Guillemot <sebastiengllmt@gmail.com>
2022-04-18 07:50:22 +0900

    Simplify cert builder

--- rust/src/builders/certificate_builder.rs
+++ rust/src/builders/certificate_builder.rs
@@ -2,6 +2,48 @@ use crate::*;
 use crate::builders::witness_builder::{InputAggregateWitnessData, PartialPlutusWitness};
 use std::collections::{HashSet};
 
+use super::witness_builder::RequiredWitnessSet;
+
+// comes from witsVKeyNeeded in the Ledger spec
+pub fn cert_required_wits(cert_enum: &Certificate, required_witnesses: &mut RequiredWitnessSet) -> () {
+    match &cert_enum.0 {
+        // stake key registrations do not require a witness
+        CertificateEnum::StakeRegistration(_cert) => (),
+        CertificateEnum::StakeDeregistration(cert) => match cert.stake_credential().kind() {
+            StakeCredKind::Script => {
+                required_witnesses.add_script_hash(&cert.stake_credential().to_scripthash().unwrap());
+            }
+            StakeCredKind::Key => {
+                required_witnesses.add_vkey_key_hash(&cert.stake_credential().to_keyhash().unwrap());
+            }
+        },
+        CertificateEnum::StakeDelegation(cert) => match cert.stake_credential().kind() {
+            StakeCredKind::Script => {
+                required_witnesses.add_script_hash(&cert.stake_credential().to_scripthash().unwrap());
+            }
+            StakeCredKind::Key => {
+                required_witnesses.add_vkey_key_hash(&cert.stake_credential().to_keyhash().unwrap());
+            }
+        },
+        CertificateEnum::PoolRegistration(cert) => {
+            for owner in &cert.pool_params().pool_owners().0 {
+                required_witnesses.add_vkey_key_hash(&owner.clone());
+            }
+            required_witnesses.add_vkey_key_hash(&cert.pool_params().operator());
+        },
+        CertificateEnum::PoolRetirement(cert) => {
+            required_witnesses.add_vkey_key_hash(&cert.pool_keyhash());
+        },
+        CertificateEnum::GenesisKeyDelegation(cert) => {
+            required_witnesses.add_vkey_key_hash(
+                &Ed25519KeyHash::from_bytes(cert.genesis_delegate_hash().to_bytes()).unwrap()
+            );
+        },
+        // no witness as there is no single core node or genesis key that posts the certificate
+        CertificateEnum::MoveInstantaneousRewardsCert(_cert) => {},
+    };
+}
+
 // comes from witsVKeyNeeded in the Ledger spec
 pub fn add_cert_vkeys(cert_enum: &Certificate, vkeys: &mut HashSet<Ed25519KeyHash>) -> Result<(), JsError> {
     match &cert_enum.0 {
@@ -39,45 +81,12 @@ pub fn add_cert_vkeys(cert_enum: &Certificate, vkeys: &mut HashSet<Ed25519KeyHas
     Ok(())
 }
 
-/// Check if the script hash matches the one in the certificate
-/// If the certificate does not require the script to sign the transaction, Ok(false) is returned
-fn check_cert_script_hash(cert_enum: &Certificate, expected_hash: &ScriptHash) -> Result<bool, JsError> {
-    match &cert_enum.0 {
-        // stake key registrations do not require a witness
-        CertificateEnum::StakeRegistration(_cert) => Ok(false),
-        CertificateEnum::StakeDeregistration(cert) => match cert.stake_credential().to_scripthash() {
-            Some(script_hash) => {
-                match *expected_hash == script_hash {
-                    true => Ok(true),
-                    false => return Err(JsError::from_str(&format!("Deregistration certificate contains wrong script hash. Expected {}, got {}.\n{:#?}", expected_hash, script_hash, cert.to_json()))),
-                }
-            },
-            None => return Err(JsError::from_str(&format!("Deregistration certificate contains public key hash. Expected script.\n{:#?}", cert.to_json()))),
-        },
-        CertificateEnum::StakeDelegation(cert) => match cert.stake_credential().to_scripthash() {
-            Some(script_hash) => {
-                match *expected_hash == script_hash {
-                    true => Ok(true),
-                    false => return Err(JsError::from_str(&format!("Delegation certificate contains wrong script hash. Expected {}, got {}.\n{:#?}", expected_hash, script_hash, cert.to_json()))),
-                }
-            },
-            None => return Err(JsError::from_str(&format!("Delegation certificate contains public key hash. Expected script.\n{:#?}", cert.to_json()))),
-        },
-        // can't register pools with scripts
-        CertificateEnum::PoolRegistration(_cert) => Ok(false),
-        CertificateEnum::PoolRetirement(_cert) => Ok(false),
-        // genesis keys are not scripts
-        CertificateEnum::GenesisKeyDelegation(_cert) => Ok(false),
-        // no witness as there is no single core node or genesis key that posts the certificate
-        CertificateEnum::MoveInstantaneousRewardsCert(_cert) => Ok(false),
-    }
-}
-
 #[wasm_bindgen]
 #[derive(Clone)]
 pub struct CertificateBuilderResult {
     cert: Certificate,
     aggregate_witness: Option<InputAggregateWitnessData>,
+    required_wits: RequiredWitnessSet,
 }
 
 #[wasm_bindgen]
@@ -94,41 +103,90 @@ impl SingleCertificateBuilder {
         }
     }
 
-    pub fn no_script(&self) -> Result<CertificateBuilderResult, JsError> {
-        let mut vkey_set = HashSet::<Ed25519KeyHash>::new();
-        add_cert_vkeys(&self.cert, &mut vkey_set)?;
+    pub fn skip_witness(&self) -> CertificateBuilderResult {
+        let mut required_wits = RequiredWitnessSet::default();
+        cert_required_wits(&self.cert, &mut required_wits);
+
+        CertificateBuilderResult {
+            cert: self.cert.clone(),
+            aggregate_witness: None,
+            required_wits: required_wits.clone(),
+        }
+    }
+
+    pub fn vkeys(&self, vkeys: &Vkeys) -> Result<CertificateBuilderResult, JsError> {
+        let mut required_wits = RequiredWitnessSet::default();
+        cert_required_wits(&self.cert, &mut required_wits);
+        let mut required_wits_left = required_wits.clone();
+
+        // the user may have provided more witnesses than required. Strip it down to just the required wits
+        let provided_wit_subset: Vec<&Vkey> = vkeys.0.iter().filter(|vkey| required_wits_left.vkeys.contains(&vkey.public_key().hash())).collect();
+        
+        // check the user provided all the required witnesses
+        provided_wit_subset.iter().for_each(|wit| { required_wits_left.vkeys.remove(&wit.public_key().hash()); });
+
+        if required_wits_left.len() > 0 {
+            return Err(JsError::from_str(&format!("Missing the following witnesses for the certificate: \n{:#?}", required_wits_left.to_str()))); 
+        }
+
         Ok(CertificateBuilderResult {
             cert: self.cert.clone(),
-            aggregate_witness: if vkey_set.len() > 0 { Some(InputAggregateWitnessData::Vkeys(vkey_set.clone())) } else { None },
+            aggregate_witness: if provided_wit_subset.len() > 0 { Some(InputAggregateWitnessData::Vkeys(provided_wit_subset.into_iter().cloned().collect())) } else { None },
+            required_wits: required_wits.clone(),
         })
     }
 
+    pub fn vkey(&self, vkey: &Vkey) -> Result<CertificateBuilderResult, JsError> {
+        self.vkeys(&Vkeys(vec![vkey.clone()]))
+    }
+
     pub fn native_script(&self, native_script: &NativeScript) -> Result<CertificateBuilderResult, JsError> {
-        let expected_hash = native_script.hash(ScriptHashNamespace::NativeScript);
-        match check_cert_script_hash(&self.cert, &expected_hash)? {
-            true => Ok(CertificateBuilderResult {
-                cert: self.cert.clone(),
-                aggregate_witness: Some(InputAggregateWitnessData::NativeScript(native_script.clone())),
-            }),
-            false => Ok(CertificateBuilderResult {
-                cert: self.cert.clone(),
-                aggregate_witness: None,
-            })
+        let mut required_wits = RequiredWitnessSet::default();
+        cert_required_wits(&self.cert, &mut required_wits);
+        let mut required_wits_left = required_wits.clone();
+
+        // the user may have provided more witnesses than required. Strip it down to just the required wits
+        let contains = required_wits_left.scripts.contains(&native_script.hash(ScriptHashNamespace::NativeScript));
+        
+        // check the user provided all the required witnesses
+        required_wits_left.scripts.remove(&native_script.hash(ScriptHashNamespace::NativeScript));
+
+        if required_wits_left.len() > 0 {
+            return Err(JsError::from_str(&format!("Missing the following witnesses for the certificate: \n{:#?}", required_wits_left.to_str()))); 
         }
+
+        let mut required_wits = RequiredWitnessSet::default();
+        cert_required_wits(&self.cert, &mut required_wits);
+
+        Ok(CertificateBuilderResult {
+            cert: self.cert.clone(),
+            aggregate_witness: if contains { Some(InputAggregateWitnessData::NativeScript(native_script.clone())) } else { None },
+            required_wits: required_wits.clone(),
+        })
     }
 
     pub fn plutus_script(&self, partial_witness: &PartialPlutusWitness) -> Result<CertificateBuilderResult, JsError> {
-        // TODO: support PlutusV2
-        let expected_hash = partial_witness.script().hash(ScriptHashNamespace::PlutusV1);
-        match check_cert_script_hash(&self.cert, &expected_hash)? {
-            true => Ok(CertificateBuilderResult {
-                cert: self.cert.clone(),
-                aggregate_witness: Some(InputAggregateWitnessData::PlutusScriptNoDatum(partial_witness.clone())),
-            }),
-            false => Ok(CertificateBuilderResult {
-                cert: self.cert.clone(),
-                aggregate_witness: None,
-            })
+        let mut required_wits = RequiredWitnessSet::default();
+        cert_required_wits(&self.cert, &mut required_wits);
+        let mut required_wits_left = required_wits.clone();
+
+        // TODO: Plutus V2
+        let script_hash = partial_witness.script().hash(ScriptHashNamespace::PlutusV1);
+
+        // the user may have provided more witnesses than required. Strip it down to just the required wits
+        let contains = required_wits_left.scripts.contains(&script_hash);
+        
+        // check the user provided all the required witnesses
+        required_wits_left.scripts.remove(&script_hash);
+
+        if required_wits_left.len() > 0 {
+            return Err(JsError::from_str(&format!("Missing the following witnesses for the certificate: \n{:#?}", required_wits_left.to_str()))); 
         }
+
+        Ok(CertificateBuilderResult {
+            cert: self.cert.clone(),
+            aggregate_witness: if contains { Some(InputAggregateWitnessData::PlutusScriptNoDatum(partial_witness.clone())) } else { None },
+            required_wits: required_wits.clone(),
+        })
     }
 }

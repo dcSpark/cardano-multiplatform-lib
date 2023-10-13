d6d00efb67096ec3a81471035fd52571f879c599
oskin1 <ilya.arcadich@gmail.com>
2023-10-13 18:21:45 +0300

    Derive Copy for fixed-len arrays.

--- rust/src/crypto.rs
+++ rust/src/crypto.rs
@@ -804,7 +804,7 @@ impl_signature!(Ed25519Signature, Vec<u8>, crypto::Ed25519);
 macro_rules! impl_hash_type {
     ($name:ident, $byte_count:expr) => {
         #[wasm_bindgen]
-        #[derive(Debug, Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
+        #[derive(Debug, Copy, Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
         pub struct $name(pub (crate) [u8; $byte_count]);
 
         // hash types are the only types in this library to not expect the entire CBOR structure.

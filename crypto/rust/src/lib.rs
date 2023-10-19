4350a84c2847a4773709f8a1b9906ea7668d2964
rooooooooob <rooooooooob@users.noreply.github.com>
2023-10-19 12:14:33 -0300

    Fix CI cause by rust updating to 1.73 (#269)
    `clippy::incorrect_partial_ord_impl_on_ord_type` was failing.
    
    In a few cases this was fixed here, but others were coming from our use
    of the `derivative` crate causing clippy to think both were
    non-canonical implementations.
    
    Issue here for derivative: mcarton/rust-derivative#115
    
    This was blanket ignored in cml-chain+cml-core to work around those.

--- crypto/rust/src/lib.rs
+++ crypto/rust/src/lib.rs
@@ -478,7 +478,7 @@ macro_rules! impl_signature {
 
         impl PartialOrd for $name {
             fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-                self.0.as_ref().partial_cmp(other.0.as_ref())
+                Some(self.cmp(other))
             }
         }
 

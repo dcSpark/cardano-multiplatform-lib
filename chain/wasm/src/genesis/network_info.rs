26a0d5ea3586ddc044532b8a0700356a933a35bf
Sebastien Guillemot <SebastienGllmt@gmail.com>
2025-01-23 18:49:04 -0700

    add wasm functions to get txid for genesis utxos (#372)

--- chain/wasm/src/genesis/network_info.rs
+++ chain/wasm/src/genesis/network_info.rs
@@ -1,6 +1,7 @@
 use crate::byron::utils::ProtocolMagic;
 use cml_core_wasm::impl_wasm_conversions;
 use wasm_bindgen::prelude::wasm_bindgen;
+use cml_crypto_wasm::TransactionHash;
 
 #[wasm_bindgen]
 pub struct NetworkInfo(cml_chain::genesis::network_info::NetworkInfo);
@@ -42,4 +43,37 @@ impl NetworkInfo {
     pub fn sancho_testnet() -> Self {
         cml_chain::genesis::network_info::NetworkInfo::sancho_testnet().into()
     }
+}
+
+#[wasm_bindgen]
+pub struct ByronGenesisRedeem(pub (crate) (cml_crypto::TransactionHash, cml_chain::byron::ByronAddress));
+
+#[wasm_bindgen]
+impl ByronGenesisRedeem {
+    pub fn new(txid: &TransactionHash, address: &crate::byron::ByronAddress) -> Self {
+        ByronGenesisRedeem((txid.clone().into(), address.clone().into()))
+    }
+
+    pub fn txid(&self) -> TransactionHash {
+        self.0.0.into()
+    }
+
+    pub fn address(&self) -> crate::byron::ByronAddress {
+        self.0.1.clone().into()
+    }
+}
+
+
+#[wasm_bindgen]
+pub fn genesis_txid_byron(pubkey: &cml_crypto_wasm::PublicKey, protocol_magic: Option<u32>) -> ByronGenesisRedeem {
+    let redeem = cml_chain::genesis::byron::parse::redeem_pubkey_to_txid(
+        &pubkey.clone().as_ref().0, 
+        protocol_magic.map(|pm| ProtocolMagic::new(pm).into())
+    );
+    ByronGenesisRedeem(redeem)
+}
+
+#[wasm_bindgen]
+pub fn genesis_txid_shelley(address: &crate::address::Address) -> TransactionHash {
+    TransactionHash::from(cml_chain::genesis::shelley::parse::redeem_address_to_txid(&address.clone().into()))
 }
